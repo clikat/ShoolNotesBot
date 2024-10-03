6757e4a564a12b0ab85418d8b4c71fa37dcee4ae
Kodoku <nikitazhigulev@yandex.ru>
2024-10-03 14:54:11 +0300

    /start
    изменено описание

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ bot = telebot.TeleBot("7756654184:AAEA4VJeseSlQBEsw0tYNO_npT_agqoMjhc")
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Здраствуйте, это телеграмм бот созданный для Школы №60 Ростова-на-Дону. Создан: @qkodoku и @tyrytuty")
+    bot.send_message(message.chat.id, "Здраствуйте, зарегистрируйтесь или войдите. /Register или /SingIn")
