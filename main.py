d830a571188bfa35c5723201e0efe80c5590fdf2
Kodoku <nikitazhigulev@yandex.ru>
2024-10-03 12:34:34 +0300

    /help
    добавлен ответ на комманду

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import time
+import sqlite3
+import telebot
 
+bot = telebot.TeleBot("7756654184:AAEA4VJeseSlQBEsw0tYNO_npT_agqoMjhc")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Здраствуйте, это телеграмм бот созданный для Школы №60 Ростова-на-Дону. Создан: @qkodoku и @tyrytuty")
